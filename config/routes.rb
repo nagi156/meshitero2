b05226ec6174f991169b86554e9300fc0b94ead7
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-07-23 08:31:44 +0000

    [Add]bootstrapとfontawesome導入

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,18 @@
 Rails.application.routes.draw do
+  devise_for :users,skip: [:passwords], controllers: {
+    registrations: "public/registrations",
+    sessions: "public/sessions"
+  }
+  scope module: :public do
+    root to: "homes#top"
+  end
+
+
+  devise_for :admins, skip: [:registrations, :passwords], contorllers: {
+    sessions: "admin/sessions"
+  }
+  namespace :admin do
+    root to: "homes#top"
+  end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
