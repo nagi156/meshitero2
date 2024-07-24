d6929bb9276859f023d8750258fd7e2e2286186c
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-07-24 10:42:18 +0000

    [Add]ユーザーログイン、サインアップ、管理者ログイン画面にアクセスできる

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,22 @@
 Rails.application.routes.draw do
+  namespace :admin do
+    get 'homes/top'
+  end
   devise_for :users,skip: [:passwords], controllers: {
     registrations: "public/registrations",
     sessions: "public/sessions"
   }
   scope module: :public do
     root to: "homes#top"
+    get "/about" => "homes#about", as: "about"
   end
 
 
-  devise_for :admins, skip: [:registrations, :passwords], contorllers: {
+  devise_for :admin, skip: [:registrations, :passwords], controllers: {
     sessions: "admin/sessions"
   }
   namespace :admin do
-    root to: "homes#top"
+    get "" => "homes#top"
   end
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
