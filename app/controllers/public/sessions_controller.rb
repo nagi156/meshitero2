bb53a43594d3da223a98366ddd3e15dd5f493832
nagi156 <158553840+nagi156@users.noreply.github.com>
2024-07-26 13:39:17 +0000

    [Add]フラッシュメッセージ、ヘッダー、フッター

--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -25,7 +25,7 @@ class Public::SessionsController < Devise::SessionsController
   end
 
   def after_sign_out_path_for(resource)
-    about_path
+    root_path
   end
 
   # If you have extra params to permit, append them to the sanitizer.
